a9f6c8394691d3fac4de7cffe5a842ccd4062fa3
gmkg1 <maheengmk@gmail.com>
2026-06-10 12:42:11 +0530

    url change

--- projects/asset-management/src/environments/environment.local-server.prod.ts
+++ projects/asset-management/src/environments/environment.local-server.prod.ts
@@ -13,7 +13,7 @@ export const environment = {
         'asset-management': 'http://localhost:4205',
     },
     publicPath: 'http://localhost:4200/',
-    baseUrl: 'https://spoke-michael-recognized-prefix.trycloudflare.com/api/',
+    baseUrl: 'https://appliance-greetings-missed-mothers.trycloudflare.com/api/',
     project: 'shell',
     baseRoute: 'kjusys',
     local: true,
--- projects/asset-management/src/environments/environment.ts
+++ projects/asset-management/src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: false,
-    apiUrl: 'https://spoke-michael-recognized-prefix.trycloudflare.com/kjusys-api/asset-management-api',
+    apiUrl: 'https://appliance-greetings-missed-mothers.trycloudflare.com/kjusys-api/asset-management-api',
     appName: 'Asset Management',
     version: '1.0.0'
 };
