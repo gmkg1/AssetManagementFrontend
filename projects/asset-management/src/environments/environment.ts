5b052add79c6b46b86c23837fc1d898c15e095ea
gmkg1 <maheengmk@gmail.com>
2026-06-10 09:36:51 +0530

    Attempt to fix lib errors

--- projects/asset-management/src/environments/environment.ts
+++ projects/asset-management/src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: false,
-    apiUrl: 'https://favour-stable-finals-tee.trycloudflare.com/kjusys-api/asset-management-api',
+    apiUrl: 'https://spoke-michael-recognized-prefix.trycloudflare.com/kjusys-api/asset-management-api',
     appName: 'Asset Management',
     version: '1.0.0'
 };
