ad55186bb2156b777d9ada8f5c9d6b48d761e869
spk1991 <surajprakask@gmail.com>
2026-06-10 14:22:51 +0530

    Minor changes

--- projects/asset-management/src/environments/environment.ts
+++ projects/asset-management/src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: false,
-    apiUrl: 'https://spoke-michael-recognized-prefix.trycloudflare.com/kjusys-api/asset-management-api',
+    apiUrl: 'https://growing-appreciation-specialties-stephanie.trycloudflare.com/kjusys-api/asset-management-api',
     appName: 'Asset Management',
     version: '1.0.0'
 };
