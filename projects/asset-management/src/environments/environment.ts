a49bfd5462181c581de728187a2e2e75a1b7db42
AshnaElsebeth <elsebethshibuashna@gmail.com>
2026-06-12 09:31:03 +0530

    Created license

--- projects/asset-management/src/environments/environment.ts
+++ projects/asset-management/src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: false,
-    apiUrl: 'https://spoke-michael-recognized-prefix.trycloudflare.com/kjusys-api/asset-management-api',
+    apiUrl: 'https://regression-burlington-connector-farmers.trycloudflare.com/kjusys-api/asset-management-api',
     appName: 'Asset Management',
     version: '1.0.0'
 };
