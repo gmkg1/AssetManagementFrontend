23c64458705a55e632f6fac139621bed9e063fd9
spk1991 <surajprakask@gmail.com>
2026-06-12 11:33:33 +0530

    Format/Font checked

--- projects/asset-management/src/environments/environment.ts
+++ projects/asset-management/src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: false,
-    apiUrl: 'https://growing-appreciation-specialties-stephanie.trycloudflare.com/kjusys-api/asset-management-api',
+    apiUrl: 'https://portfolio-mae-governing-maximum.trycloudflare.com/kjusys-api/asset-management-api',
     appName: 'Asset Management',
     version: '1.0.0'
 };
