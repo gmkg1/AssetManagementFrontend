fd8dea25cf04c7a368b52ffefc0c447c10db9099
punith <punithrajm06@gmail.com>
2026-06-15 14:31:31 +0530

    hh

--- projects/asset-management/src/environments/environment.ts
+++ projects/asset-management/src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: false,
-    apiUrl: 'http://localhost:8080/kjusys-api/asset-management-api',
+    apiUrl: 'https://highway-over-concluded-period.trycloudflare.com/kjusys-api/asset-management-api',
     appName: 'Asset Management',
     version: '1.0.0'
 };
