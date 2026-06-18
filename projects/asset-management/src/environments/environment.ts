613c269e4b80abb33b896caffdf3614ec56a8fff
gmkg1 <maheengmk@gmail.com>
2026-06-18 09:18:19 +0530

    local

--- projects/asset-management/src/environments/environment.ts
+++ projects/asset-management/src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: false,
-    apiUrl: 'https://vampire-outputs-hardware-leisure.trycloudflare.com/kjusys-api/asset-management-api',
+    apiUrl: 'http://localhost:8080/kjusys-api/asset-management-api',
     appName: 'Asset Management',
     version: '1.0.0'
 };
