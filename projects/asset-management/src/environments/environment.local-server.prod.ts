1a12b768d642465255fce8556b0ffd83c5f80303
punith <punithrajm06@gmail.com>
2026-06-18 11:32:27 +0530

    haha

--- projects/asset-management/src/environments/environment.local-server.prod.ts
+++ projects/asset-management/src/environments/environment.local-server.prod.ts
@@ -13,7 +13,7 @@ export const environment = {
         'asset-management': 'http://localhost:4205',
     },
     publicPath: 'http://localhost:4200/',
-    baseUrl: 'https://vampire-outputs-hardware-leisure.trycloudflare.com/api/',
+    baseUrl: 'https://adjustments-plenty-roles-grid.trycloudflare.com/api/',
     project: 'shell',
     baseRoute: 'kjusys',
     local: true,
--- projects/asset-management/src/environments/environment.ts
+++ projects/asset-management/src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: false,
-    apiUrl: 'https://vampire-outputs-hardware-leisure.trycloudflare.com/kjusys-api/asset-management-api',
+    apiUrl: 'https://adjustments-plenty-roles-grid.trycloudflare.com/kjusys-api/asset-management-api',
     appName: 'Asset Management',
     version: '1.0.0'
 };
