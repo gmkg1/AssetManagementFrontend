f190924cba01431c84c00300d6be9cf4324062be
spk1991 <surajprakask@gmail.com>
2026-06-18 12:25:41 +0530

    ..

--- projects/asset-management/src/environments/environment.ts
+++ projects/asset-management/src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: false,
-    apiUrl: 'https://lives-mark-interview-thin.trycloudflare.com/kjusys-api/asset-management-api',
+    apiUrl: 'https://adjustments-plenty-roles-grid.trycloudflare.com/kjusys-api/asset-management-api',
     appName: 'Asset Management',
     version: '1.0.0'
 };
